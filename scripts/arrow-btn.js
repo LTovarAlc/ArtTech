c3fa1ca58fec3711abe42de664796af70a746b09
Luis Tovar <130521074+LTovarAlc@users.noreply.github.com>
2023-07-26 12:44:36 -0600

    section connections ready

---
+++ scripts/arrow-btn.js
@@ -0,0 +1,17 @@
+// buttons show text
+
+const btnShowText = document.querySelectorAll('.btn__show-text');
+
+btnShowText.forEach((btnShowText) =>{
+    btnShowText.addEventListener('click', function(){
+         // Encontrar el párrafo correspondiente al botón actual
+        const parentDiv = this.parentElement; // Elemento padre (div) del botón actual
+        const descriptionParagraph = parentDiv.querySelector('.conections__description');
+
+        if (descriptionParagraph.style.display === 'none') {
+            descriptionParagraph.style.display = 'block';
+          } else {
+            descriptionParagraph.style.display = 'none';
+          }
+    })
+})
